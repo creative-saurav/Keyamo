ea1521d037489a6abfe4ed3ff93e748bba1c80e8
Subir Sarkar Saurav <subirsarkar90283@gmail.com>
2022-06-12 15:50:04 +0600

    add member

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,6 +98,9 @@ $(document).ready(function(){
                 0:{
                     items:1,
                 },
+                576:{
+                    items:2,
+                },
                 767:{
                     items:2,
                 },
